d6a383ae9980f1238172f957454c7845729914a0
MAY <81168645+ayedm1@users.noreply.github.com>
2026-01-25 08:25:45 +0100

    .

--- scripts/build_samples_rtos.sh
+++ scripts/build_samples_rtos.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-$(dirname `realpath $0`)/../test/cmake/usbx/regression_samples_rtos/run.sh build $@
+$(dirname `realpath $0`)/../test/cmake/regression_samples_rtos/run.sh build $@
--- test/cmake/regression_samples_rtos/run.sh
+++ test/cmake/regression_samples_rtos/run.sh
@@ -3,13 +3,14 @@
 cd $(dirname $0)
 
 # Checkout externals
-[ -d ../../../externals ] || mkdir -p ../../../externals
-[ -d ../../../externals/threadx/.git ] || git clone https://github.com/eclipse-threadx/threadx.git ../../../externals/threadx
+[ -d externals ] || mkdir ../../externals
+git clone https://github.com/eclipse-threadx/threadx.git ../../externals/threadx
+
 
 # Add junit output for ctest generation
-if ! grep -q "\-\-output\-junit \$1.xml" ../../../externals/threadx/scripts/cmake_bootstrap.sh; then
-  sed -i 's/ctest $parallel --timeout 1000 -O $1.txt/& --output-junit $1.xml/g' ../../../externals/threadx/scripts/cmake_bootstrap.sh
+if ! grep -q "\-\-output\-junit \$1.xml" ../../externals/threadx/scripts/cmake_bootstrap.sh; then
+  sed -i 's/ctest $parallel --timeout 1000 -O $1.txt/& --output-junit $1.xml/g' ../../externals/threadx/scripts/cmake_bootstrap.sh
 fi
 
-[ -f .run.sh ] || ln -sf ../../../externals/threadx/scripts/cmake_bootstrap.sh .run.sh
+[ -f .run.sh ] || ln -sf ../../externals/threadx/scripts/cmake_bootstrap.sh .run.sh
 ./.run.sh $*
